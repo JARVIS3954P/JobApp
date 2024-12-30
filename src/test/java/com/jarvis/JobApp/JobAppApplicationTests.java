ed9b67ce2e7a44def5eccb8f4f1c3c295437d336
JARVIS3954P <gaintacolyte9@gmail.com>
2024-12-31 02:48:25 +0530

    Update

---
+++ src/test/java/com/jarvis/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
